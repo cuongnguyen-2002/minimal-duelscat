327986ada9e30068fb51b36c8bf229a290a85289
cuongnguyen-2002 <nguyenduccuong.072002@gmail.com>
2026-06-14 12:03:17 +0700

    add picksongscreen

--- Assets/Game/UI/HomeScreen.cs
+++ Assets/Game/UI/HomeScreen.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 public class HomeScreen : UIBase
 {
     [SerializeField] private Button _tabScreen;
+    [SerializeField] private RectTransform _leftHand;
+    [SerializeField] private RectTransform _rightHand;
 
     public override void Init(UIManager manager)
     {
@@ -16,13 +18,16 @@ public class HomeScreen : UIBase
     {
         base.Show();
         _canvasGroup.DOFade(1, 0);
-        
+        _leftHand.DOAnchorPosX(-365, 1f).SetLoops(-1 ,LoopType.Yoyo).SetEase(Ease.Linear);
+        _rightHand.DOAnchorPosX(365, 1f).SetLoops(-1 ,LoopType.Yoyo).SetEase(Ease.Linear);
     }
 
     public override void Hide()
     {
         base.Hide();
         _canvasGroup.DOFade(0, 1);
+        _leftHand.DOKill();
+        _rightHand.DOKill();
     }
 
     public void OnStartGame()
